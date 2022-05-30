c5fefb667d7a4a29db92393c816455f0ae5b151f
Fernando <fernandondin@ciencias.unam.mx>
2022-05-30 00:47:16 -0500

    eliminar

--- src/main/java/com/example/Proyecto/controlador/Controlador.java
+++ src/main/java/com/example/Proyecto/controlador/Controlador.java
@@ -1,9 +1,9 @@
 package com.example.Proyecto.controlador;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedList;
@@ -11,6 +11,7 @@ import java.util.LinkedList;
 @Controller
 public class Controlador {
     private LinkedList<Nota> notas = new LinkedList<>();
+    private  int id=0;
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("notas",notas);
@@ -24,9 +25,17 @@ public class Controlador {
         String texto = request.getParameter("contenido");
         String colorTitulo = request.getParameter("color-titulo");
         String colorContenido= request.getParameter("color-contenido");
-        Nota nota = new Nota(titulo,texto,colorTitulo,colorContenido);
+        Nota nota = new Nota(titulo,texto,colorTitulo,colorContenido,id++);
         notas.add(nota);
         System.out.println(colorTitulo);
+        model.addAttribute("notas",notas);
+        return "notas";
+    }
+    @GetMapping(value="/eliminar/{titulo}/{contenido}/{colorTitulo}/{colorContenido}/{id}")
+    public String deleteBook(@PathVariable String titulo,@PathVariable String contenido,@PathVariable String colorTitulo,
+                             @PathVariable String colorContenido,@PathVariable Integer id,Model model) {
+        Nota eliminada = new Nota(titulo,contenido,colorTitulo,colorContenido,id);
+        notas.remove(eliminada);
         model.addAttribute("notas",notas);
         return "notas";
     }
--- src/main/java/com/example/Proyecto/controlador/Nota.java
+++ src/main/java/com/example/Proyecto/controlador/Nota.java
@@ -8,10 +8,12 @@ public class Nota {
     private String contenido;
     private String colorTitulo;
     private String colorContenido;
-    public Nota(String titulo, String contenido, String colorTitulo, String colorContenido){
+    private int id;
+    public Nota(String titulo, String contenido, String colorTitulo, String colorContenido,int id){
         this.titulo=titulo;
         this.contenido=contenido;
         this.colorTitulo=colorTitulo;
         this.colorContenido=colorContenido;
+        this.id=id;
     }
 }
